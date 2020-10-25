09e3ae5706e0c0b184b1ab65750d1365145d994d
unknown <brandynodoyo@gmail.com>
2020-10-26 01:47:01 +0300

    footer complete

---
+++ frontend/src/components/UI/List/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function List({ children, classes }) {
+  return <ul className={classes}>{children}</ul>;
+}
+
+export default List;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import LoginRegister from "../components/LoginRegister";
 import MobileMenu from "../components/MobileMenu";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header />
       <MobileMenu />
       <LoginRegister />
+      <Footer />
     </>
   );
 };
