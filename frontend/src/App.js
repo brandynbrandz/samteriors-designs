077245c5b04e5d331335a641cc08e476838e3641
unknown <brandynodoyo@gmail.com>
2020-10-27 22:35:42 +0300

    basic page router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Error404 from "./pages/Error404";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL + "/"}`}
+            component={Home}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL + "/about"}`}
+            component={About}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL + "/services"}`}
+            component={Services}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL + "/team"}`}
+            component={Team}
+          />
+          <Route
+            exact
+            path={`${process.env.PUBLIC_URL + "/contact"}`}
+            component={Contact}
+          />
           <Route component={Error404} />
         </Switch>
       </Router>
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+const About = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default About;
