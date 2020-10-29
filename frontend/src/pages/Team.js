1ef56d5d542f21650cd3704f63f1133351a628ff
unknown <brandynodoyo@gmail.com>
2020-10-29 03:48:56 +0300

    teams complete

--- frontend/src/pages/Team.js
+++ frontend/src/pages/Team.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
+import PageHeader from "../components/PageHeader";
+import About from "../components/About/page";
+import TeamMember from "../templates/Team";
+import BrandLogo from "../components/BrandLogo";
 import LoginRegister from "../components/LoginRegister";
 import MobileMenu from "../components/MobileMenu";
 import CallToAction from "../components/CallToAction";
@@ -10,7 +14,21 @@ const Team = () => {
   return (
     <>
       <Header />
-      <Funfact />
+      <PageHeader
+        bgImg={process.env.PUBLIC_URL + "../assets/img/page-header.jpg"}
+        title="KNOW ABOUT BRANDZ-INTERIOR THE ULTIMATE TEAM"
+        content="Brandz-Interior always try to provide the best Business Solutions for Clients to grow up their Business very sharply and smoothly."
+      />
+      <About
+        title={"Our Team"}
+        heading="Meet Our <br/> Expert Member"
+        thumb={process.env.PUBLIC_URL + "/assets/img/about.jpg"}
+        content="<b>Brandz-Interior</b> always try to provide the best Business Solutions for Clinets to grow up their Business very sharply and smoothly. We voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."
+      />
+
+      <TeamMember />
+      <BrandLogo />
+      <Funfact classes="sp-top" />
       <CallToAction />
       <Footer />
       <MobileMenu />
---
+++ frontend/src/templates/Team/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import teamData from "../../data/Team/team";
+import TeamMember from "../../components/Team//Member";
+
+const TeamPage = () => {
+  return (
+    <div className="team-page-area-wrapper bg-offwhite sp-y sm-top">
+      <div className="container">
+        <div className="row mtn-30">
+          {teamData.map((member) => (
+            <TeamMember
+              key={member.id}
+              type={"page"}
+              id={member.id}
+              name={member.name}
+              designation={member.designation}
+              profilePic={member.profilePic}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamPage;
